c1335c41eff8f6c7a7a7d127c34bcb308b7df0d8
angelpang08 <angelpang08@gmail.com>
2019-01-25 18:06:12 -0800

    1

--- profileController.js
+++ profileController.js
@@ -6,8 +6,8 @@
             {name: 'Firefox', percent: 8.71}
         ];
         var inner_dataset = [
-            {name: 0, percent: 10},
-            {name: 1, percent: 30},
+            {name: "kk", percent: 30},
+            {name:"gg", percent: 30},
             
         ];
 
@@ -15,44 +15,88 @@
             .value(function (d) {
                 return d.percent
             })
-            .sort(null);
+            .sort(null)
+            ;
         //.padAngle(.03);
 
-        var w = 300, h = 300;
+        var w = 800, h = 600;
+        var Radius= Math.min(w,h)*0.8;
+        var outerRadius = Radius/2;
+        var innerRadius = Radius/3;
 
-        var outerRadius = w / 2;
-        var innerRadius = 100;
         var color = d3.scale.category20b();
         var arc = d3.svg.arc()
             .outerRadius(outerRadius)
             .innerRadius(innerRadius);
 
+        var arcOver = d3.svg.arc().outerRadius(outerRadius + 20).innerRadius(innerRadius +20);
+
+         var arc2 = d3.svg.arc()
+                .outerRadius(Radius/3)
+                .innerRadius(Radius/8)
+                // .startAngle(function(d) { return d.startAngle + Math.PI/3; })
+                // .endAngle(function(d) { return d.endAngle + Math.PI/3; });
+
         var svg_donut = d3.select("#donut-chart")
             .append("svg")
             .attr({
-                preserveAspectRatio: "xMidYMid meet",
-                viewBox: "0 0 800 800",
+                //width: w,
+                //height: h,
+                 preserveAspectRatio: "xMidYMid meet",
+                viewBox: ("0 0 "+ w +" " +h),
                 class: 'shadow'
-            }).append('g')
+            })
+            .append('g')
             .attr({
                 transform: 'translate(' + w / 2 + ',' + h / 2 + ')'
             });
-        var path = svg_donut.selectAll('path.outer')
+
+        var g_arc=svg_donut.selectAll('.arc')
             .data(pie(outer_dataset ))
             .enter()
-            .append('path')
+            .append("g")
+            .attr('class','arc')
+            .on("mouseenter", function (d) {
+                d3.select(this)
+                    .attr("stroke", "white")
+                    .transition()
+                    .duration(200)
+                    .attr("d", arcOver);
+                    
+            })
+            .on("mouseleave", function (d) {
+                 d3.select(this).transition()
+                     .duration(200)
+                     .attr("d", arc);
+             })
+            ;
+
+        g_arc.append('path')
             .attr('class', 'outer')
+            .attr("stroke", "#fff")
             .attr({
                 d: arc,
                 fill: function (d, i) {
                 	
                     return color(d.data.name);
                 }
-            });
+            })
+            ;
 
-        var arc2 = d3.svg.arc()
-            .outerRadius(100)
-            .innerRadius(50);
+                
+        g_arc.append("text").attr("class","out")
+                .attr("transform", function(d) { 
+                        var midAngle = d.startAngle/2 + d.endAngle/2 <= Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI ;
+                        return "translate(" + arc.centroid(d)[0] + "," + arc.centroid(d)[1] + ") rotate(-90) rotate(" + (midAngle * 180/Math.PI) + ")";
+                 })
+                .attr("dy", ".35em")
+                .attr("text-anchor","middle")
+                .text(function(d){
+                    //console.log(d.data.name);
+                    return d.data.name+d.data.percent;
+                }).style("fill", "black").style('opacity', 1);
+
+       
 
         
 
@@ -69,28 +113,78 @@
             })
         ;
 
-            svg_donut.selectAll("text.out")
-                .data(pie(outer_dataset)).enter()
-                .append("text").attr("class","out")
-			    .attr("transform", function(d){
-			        return "translate(" + arc.centroid(d) + ")";
-			    })
-			    .attr("text-anchor","middle")
-			    .text(function(d){
-                    console.log(d.data.name);
-			        return d.data.name+d.data.percent;
-			    }).style("fill", "black").style('opacity', 1);
+        function midAngle(d){
+            return d.startAngle-(d.endAngle-d.startAngle)/2;
+        }
 
-        svg_donut.selectAll("text.in")
+       var outerArc=d3.svg.arc().outerRadius(w/2*1.5).innerRadius(w/2*1.1);
+
+
+
+
+        var text_in=svg_donut.selectAll("text.in")
                 .data(pie(inner_dataset)).enter()
                 .append("text").attr("class","in")
-			    .attr("transform", function(d){
-			        return "translate(" + arc2.centroid(d) + ")";
-			    })
-			    .attr("text-anchor","middle")
-			    .append("p").text(function(d){
-                    console.log(d.data.name);
-			        return (d.data.name);
-			    });
+                .attr("transform", function(d) { 
+                        var midAngle = d.startAngle/2 + d.endAngle/2  <= Math.PI ? d.startAngle/2 + d.endAngle/2 : d.startAngle/2  + d.endAngle/2 + Math.PI ;
+                        return "translate(" + arc2.centroid(d)[0] + "," + arc2.centroid(d)[1] + ") rotate(-90) rotate(" + (midAngle * 180/Math.PI) + ")";
+                 })
+                .attr("dy", ".35em")
+                .attr("text-anchor","middle")
+                .text(function(d){
+                    return d.data.name;
+                }).style("fill", "black").style('opacity', 1);
+
+              //  var radius=w/2;
+
+        // text_out.transition().attrTween("transform",function(d){
+        //         this._current = this._current || d;
+        //         var interpolate = d3.interpolate(this._current, d);
+        //         this._current = interpolate(0);
+                
+        //         return function (t) {
+        //             var d2 = interpolate(t);
+        //             //获取文本内容在外圆的中心位置坐标
+        //             var pos = outerArc.centroid(d2);
+        //             //然后将文本内容进行左右平移，移动到固定的radius长度，方向由饼图中心角度是否大于180度决定。
+        //             pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+        //             return "translate(" + pos + ")";
+        //         };
+        //     }).styleTween("text-anchor", function (d) {
+        //             this._current = this._current || d;
+        //             var interpolate = d3.interpolate(this._current, d);
+        //             this._current = interpolate(0);
+        //             return function (t) {
+        //                 var d2 = interpolate(t);
+        //                 //判断文本的锚点位置
+        //                 return midAngle(d2) < Math.PI ? "start" : "end";
+        //             };
+        //        });
+
+
+            /* ------- 添加引导线 -------*/
+/*
+            var polyline = svg.select(".lines").selectAll("polyline")
+                .data(pie(data), key);
+
+            polyline.enter()
+                .append("polyline");
+
+            polyline.transition().duration(1000)
+                .attrTween("points", function (d) {
+                    this._current = this._current || d;
+                    var interpolate = d3.interpolate(this._current, d);
+                    this._current = interpolate(0);
+                    return function (t) {
+                        var d2 = interpolate(t);
+                        var pos = outerArc.centroid(d2);
+                        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+                        //折线从饼图文本中心位置开始，到饼图外圆文本的中心位置，再到文本的显示位置
+                        return [arc.centroid(d2), outerArc.centroid(d2), pos];
+                    };
+                });
+
+*/
+
 
 
