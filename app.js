290ec6db0e420985a55b60303490559d5b1f67e5
angelpang08 <angelpang08@gmail.com>
2019-02-11 23:59:18 -0800

    d

--- app.js
+++ app.js
@@ -19,17 +19,18 @@
       id: $scope.newchoice.id,
       name: $scope.newchoice.name});
 
-   }
+     console.log( Number.isInteger($scope.newchoice.id) , angular.isNumber($scope.newchoice.id) );
+  }
+
+   
 
 
     var ip= "0.1.1.001/02"
    
     //var reg =  /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/ 
-    var reg=/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)(\/([0-2]?\d|3[0-2]))?$/    
+    var reg=/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)(\/([0-2]?\d|3[0-2]))?$/ 
+
     $scope.newchoice.id=(reg.test(ip));
-        
-  
-   
-   
+
    
  });
